55ca307c01000765b2ad1c3eb631c4588ac7869b
Rodolfo Orozco Vasquez <44987991+rorozco1997@users.noreply.github.com>
2024-07-30 23:47:27 -0700

    very rough draft. ugly code. need to test

--- src/EffectiveCSharp.Analyzers/PreferMemberInitializersToAssignmentStatementsAnalyzer.cs
+++ src/EffectiveCSharp.Analyzers/PreferMemberInitializersToAssignmentStatementsAnalyzer.cs
@@ -19,6 +19,8 @@ public class PreferMemberInitializersToAssignmentStatementsAnalyzer : Diagnostic
         isEnabledByDefault: true,
         helpLinkUri: $"https://github.com/rjmurillo/EffectiveCSharp.Analyzers/blob/{ThisAssembly.GitCommitId}/docs/{Id}.md");
 
+    private readonly IDictionary<string, (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments)> _memberInitializerCandidates = new Dictionary<string, (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments)>(StringComparer.Ordinal);
+
     /// <inheritdoc />
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
@@ -27,11 +29,119 @@ public class PreferMemberInitializersToAssignmentStatementsAnalyzer : Diagnostic
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
         context.EnableConcurrentExecution();
-        context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ConstructorDeclaration);
+        context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
     }
 
     private void AnalyzeNode(SyntaxNodeAnalysisContext context)
     {
-        // TODO
+        ClassDeclarationSyntax classSyntaxNode = (ClassDeclarationSyntax)context.Node;
+
+        // Check in every constructor if there are member initializer candidates
+        foreach (ConstructorDeclarationSyntax constructor in classSyntaxNode.DescendantNodes().OfType<ConstructorDeclarationSyntax>())
+        {
+            FindMemberInitializerCandidates(context, constructor);
+        }
+
+        foreach (KeyValuePair<string, (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments)> memberInitializerCandidate in _memberInitializerCandidates)
+        {
+            if (memberInitializerCandidate.Value.NumberOfAssignments == 1)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, memberInitializerCandidate.Value.AssignmentNode.GetLocation()));
+            }
+        }
+    }
+
+    private void FindMemberInitializerCandidates(SyntaxNodeAnalysisContext context, ConstructorDeclarationSyntax constructor)
+    {
+        SeparatedSyntaxList<ParameterSyntax> arguments = constructor.ParameterList.Parameters;
+
+        if (arguments.Count == 0)
+        {
+            // If the constructor has no arguments, we can consider all fields as candidates
+            HandleEmptyArgumentsList(constructor, context.SemanticModel, context.CancellationToken);
+        }
+        else
+        {
+            // If we can only add literal field assignments and invocation expressions that do not reference parameters
+            // The analyzer does not support more complex scenarios such as whether the argument list contains something that depends on a parameter
+            HandleArgumentsList(constructor, context.SemanticModel, context.CancellationToken);
+        }
+    }
+
+    private void HandleEmptyArgumentsList(ConstructorDeclarationSyntax constructor, SemanticModel semanticModel, CancellationToken cancellationToken)
+    {
+        foreach (AssignmentExpressionSyntax assignment in constructor.DescendantNodes().OfType<AssignmentExpressionSyntax>())
+        {
+            if (assignment.Left is IdentifierNameSyntax identifierName &&
+                semanticModel.GetSymbolInfo(identifierName, cancellationToken: cancellationToken).Symbol is IFieldSymbol fieldSymbol)
+            {
+                if (_memberInitializerCandidates.TryGetValue(fieldSymbol.Name, out (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments) existingCandidate))
+                {
+                    existingCandidate.NumberOfAssignments++;
+                }
+                else
+                {
+                    _memberInitializerCandidates.Add(fieldSymbol.Name, (assignment, 1));
+                }
+            }
+        }
+    }
+
+    private void HandleArgumentsList(ConstructorDeclarationSyntax constructor, SemanticModel semanticModel, CancellationToken cancellationToken)
+    {
+        foreach (AssignmentExpressionSyntax assignment in constructor.DescendantNodes().OfType<AssignmentExpressionSyntax>())
+        {
+            if (assignment.Left is IdentifierNameSyntax identifierName
+                    && semanticModel.GetSymbolInfo(identifierName, cancellationToken: cancellationToken).Symbol is IFieldSymbol fieldSymbol
+                    && semanticModel.GetOperation(assignment.Right, cancellationToken) is not IParameterReferenceOperation)
+            {
+                switch (assignment.Right)
+                {
+                    case LiteralExpressionSyntax:
+                        {
+                            if (_memberInitializerCandidates.TryGetValue(fieldSymbol.Name, out (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments) existingCandidate))
+                            {
+                                existingCandidate.NumberOfAssignments++;
+                            }
+                            else
+                            {
+                                _memberInitializerCandidates.Add(fieldSymbol.Name, (assignment, 1));
+                            }
+                        }
+
+                        break;
+
+                    case InvocationExpressionSyntax invocationExpressionSyntax:
+                        {
+                            bool canAdd = true;
+                            foreach (IdentifierNameSyntax argument in invocationExpressionSyntax.ArgumentList.DescendantNodes().OfType<IdentifierNameSyntax>())
+                            {
+                                if (semanticModel.GetOperation(argument, cancellationToken) is IParameterReferenceOperation)
+                                {
+                                    canAdd = false;
+                                    break;
+                                }
+                            }
+
+                            if (canAdd)
+                            {
+                                if (_memberInitializerCandidates.TryGetValue(fieldSymbol.Name, out (AssignmentExpressionSyntax AssignmentNode, int NumberOfAssignments) existingCandidate))
+                                {
+                                    existingCandidate.NumberOfAssignments++;
+                                }
+                                else
+                                {
+                                    _memberInitializerCandidates.Add(fieldSymbol.Name, (assignment, 1));
+                                }
+                            }
+                        }
+
+                        break;
+
+                    default:
+                        continue;
+                }
+            }
+        }
     }
 }
