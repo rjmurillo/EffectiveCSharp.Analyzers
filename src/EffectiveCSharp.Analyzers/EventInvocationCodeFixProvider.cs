9c0ac17b611909ece964a0ee6dd6a44265464f0f
Richard Murillo <rjmurillo@gmail.com>
2024-08-08 16:03:20 -0700

    Code fix for ECS0008

---
+++ src/EffectiveCSharp.Analyzers/EventInvocationCodeFixProvider.cs
@@ -0,0 +1,130 @@
+﻿namespace EffectiveCSharp.Analyzers;
+
+/// <summary>
+/// A <see cref="CodeFixProvider"/> that provides a code fix for the <see cref="EventInvocationAnalyzer"/>.
+/// </summary>
+/// <seealso cref="Microsoft.CodeAnalysis.CodeFixes.CodeFixProvider" />
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(EventInvocationCodeFixProvider))]
+[Shared]
+public class EventInvocationCodeFixProvider : CodeFixProvider
+{
+    private static readonly string Title = "Use null-conditional operator";
+
+    /// <inheritdoc />
+    public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DiagnosticIds.UseNullConditionalOperatorForEventInvocations);
+
+    /// <inheritdoc />
+    public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    /// <inheritdoc />
+    public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        SyntaxNode? root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        Diagnostic diagnostic = context.Diagnostics[0];
+        TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+        SyntaxNode? node = root?.FindNode(diagnosticSpan);
+        InvocationExpressionSyntax? invocationExpression = node?.FirstAncestorOrSelf<InvocationExpressionSyntax>();
+        IfStatementSyntax? ifStatement = node?.FirstAncestorOrSelf<IfStatementSyntax>();
+
+        if (invocationExpression != null)
+        {
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => UseNullConditionalOperatorAsync(context.Document, invocationExpression, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+        else if (ifStatement != null)
+        {
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => UseNullConditionalOperatorAsync(context.Document, ifStatement, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+    }
+
+    private static async Task<Document> UseNullConditionalOperatorAsync(Document document, InvocationExpressionSyntax invocationExpression, CancellationToken cancellationToken)
+    {
+        DocumentEditor? editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+        IdentifierNameSyntax identifierName = (IdentifierNameSyntax)invocationExpression.Expression;
+        ConditionalAccessExpressionSyntax newInvocation = SyntaxFactory.ConditionalAccessExpression(
+            identifierName,
+            SyntaxFactory.InvocationExpression(
+                SyntaxFactory.MemberBindingExpression(SyntaxFactory.IdentifierName("Invoke")),
+                invocationExpression.ArgumentList));
+
+        editor.ReplaceNode(invocationExpression, newInvocation);
+        return editor.GetChangedDocument();
+    }
+
+    private static async Task<Document> UseNullConditionalOperatorAsync(Document document, IfStatementSyntax ifStatement, CancellationToken cancellationToken)
+    {
+        DocumentEditor? editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+
+        // Check if the if statement follows the pattern: if (handler != null) handler(this, counter);
+        ExpressionStatementSyntax? expressionStatement = ifStatement.Statement as ExpressionStatementSyntax;
+
+        if (expressionStatement?.Expression is not InvocationExpressionSyntax invocationExpression)
+        {
+            return document;
+        }
+
+        // Identify the event handler variable (handler in this case)
+        IdentifierNameSyntax? identifierName = invocationExpression.Expression as IdentifierNameSyntax
+                                               ?? (invocationExpression.Expression as MemberAccessExpressionSyntax)?.Expression as IdentifierNameSyntax;
+
+        if (identifierName == null)
+        {
+            return document;
+        }
+
+        // Attempt to find a preceding variable declaration if it exists
+        BlockSyntax? blockSyntax = ifStatement.Parent as BlockSyntax;
+        LocalDeclarationStatementSyntax? variableDeclaration = blockSyntax?.Statements
+            .OfType<LocalDeclarationStatementSyntax>()
+            .FirstOrDefault(v => v.Declaration.Variables.Any(var => var.Identifier.Text == identifierName.Identifier.Text));
+
+        if (variableDeclaration == null)
+        {
+            // If there is no preceding variable declaration, replace the if statement directly
+            ExpressionStatementSyntax newInvocation = SyntaxFactory.ExpressionStatement(
+                    SyntaxFactory.ConditionalAccessExpression(
+                        identifierName,
+                        SyntaxFactory.InvocationExpression(
+                            SyntaxFactory.MemberBindingExpression(SyntaxFactory.IdentifierName("Invoke")),
+                            invocationExpression.ArgumentList)))
+                .WithLeadingTrivia(ifStatement.GetLeadingTrivia())
+                .WithTrailingTrivia(ifStatement.GetTrailingTrivia());
+
+            editor.ReplaceNode(ifStatement, newInvocation);
+            return editor.GetChangedDocument();
+        }
+
+        // Get the actual event handler (e.g., Updated)
+        if (variableDeclaration.Declaration.Variables.First().Initializer?.Value is IdentifierNameSyntax eventHandlerIdentifier)
+        {
+            // Create the null-conditional invocation using the event handler directly
+            ExpressionStatementSyntax newInvocation = SyntaxFactory.ExpressionStatement(
+                    SyntaxFactory.ConditionalAccessExpression(
+                        eventHandlerIdentifier,
+                        SyntaxFactory.InvocationExpression(
+                            SyntaxFactory.MemberBindingExpression(SyntaxFactory.IdentifierName("Invoke")),
+                            invocationExpression.ArgumentList)))
+                .WithLeadingTrivia(ifStatement.GetLeadingTrivia())
+                .WithTrailingTrivia(ifStatement.GetTrailingTrivia());
+
+            // Remove the variable declaration
+            editor.RemoveNode(variableDeclaration);
+
+            // Replace the if statement with the new invocation
+            editor.ReplaceNode(ifStatement, newInvocation);
+
+            return editor.GetChangedDocument();
+        }
+
+        return document;
+    }
+}
--- tests/EffectiveCSharp.Analyzers.Tests/EventInvocationTests.cs
+++ tests/EffectiveCSharp.Analyzers.Tests/EventInvocationTests.cs
@@ -1,4 +1,6 @@
-﻿using Verifier = EffectiveCSharp.Analyzers.Tests.Helpers.AnalyzerVerifier<EffectiveCSharp.Analyzers.EventInvocationAnalyzer>;
+﻿using Xunit.Abstractions;
+using CodeFixVerifier = EffectiveCSharp.Analyzers.Tests.Helpers.AnalyzerAndCodeFixVerifier<EffectiveCSharp.Analyzers.EventInvocationAnalyzer, EffectiveCSharp.Analyzers.EventInvocationCodeFixProvider>;
+using Verifier = EffectiveCSharp.Analyzers.Tests.Helpers.AnalyzerVerifier<EffectiveCSharp.Analyzers.EventInvocationAnalyzer>;
 
 namespace EffectiveCSharp.Analyzers.Tests;
 
@@ -11,16 +13,24 @@ namespace EffectiveCSharp.Analyzers.Tests;
 #pragma warning disable IDE0028 // We cannot simply object creation on TheoryData because we need to convert from object[] to string, the way it is now is cleaner
 #pragma warning disable AsyncFixer01
 
-
 public class EventInvocationTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public EventInvocationTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     public static TheoryData<string, string> TestData()
     {
         TheoryData<string> data = new()
         {
             // When no event handlers have been attached to the Updated event
             // the code will throw NRE
-            "Updated(this, counter);",
+            """
+            {|ECS0008:Updated(this, counter)|};
+            """,
 
             // Developers need to wrap any event invocation in a check
             // This code has a bug: the null check can pass but the event
@@ -65,5 +75,117 @@ public class EventInvocationTests
             }
             """,
             referenceAssemblyGroup);
+    }
+
+    // The test cases are broken out as Facts instead of a Theory because the white space is super fiddly
+    [Fact]
+    public async Task Existing_Known_Good_Pattern()
+    {
+        const string testCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    var handler = Updated;
+                    {|ECS0008:if (handler != null)
+                        handler(this, counter);|}
+                }
+            }
+            """;
+
+        const string fixedCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    Updated?.Invoke(this, counter);
+                }
+            }
+            """;
+
+        await CodeFixVerifier.VerifyCodeFixAsync(testCode, fixedCode, ReferenceAssemblyCatalog.Latest);
+    }
+
+    [Fact]
+    public async Task Checking_for_null_against_the_EventHandler_directly()
+    {
+        const string testCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    {|ECS0008:if (Updated != null)
+                        Updated(this, counter);|}
+                }
+            }
+            """;
+
+        const string fixedCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    Updated?.Invoke(this, counter);
+                }
+            }
+            """;
+
+        await CodeFixVerifier.VerifyCodeFixAsync(testCode, fixedCode, ReferenceAssemblyCatalog.Latest);
+    }
+
+    [Fact]
+    public async Task Calling_the_event_handler_directly()
+    {
+        const string testCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    {|ECS0008:Updated(this, counter)|};
+                }
+            }
+            """;
+
+        const string fixedCode =
+            """
+            public class EventSource
+            {
+                private EventHandler<int> Updated;
+                private int counter;
+
+                public void RaiseUpdates()
+                {
+                    counter++;
+                    Updated?.Invoke(this, counter);
+                }
+            }
+            """;
+
+        await CodeFixVerifier.VerifyCodeFixAsync(testCode, fixedCode, ReferenceAssemblyCatalog.Latest);
     }
 }
