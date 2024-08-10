9cd02c0613ffd84782f5812674281c9f3fbeafb1
Richard Murillo <rjmurillo@gmail.com>
2024-08-09 19:06:14 -0700

    Add Xunit.Abstractions to global namespace

--- tests/EffectiveCSharp.Analyzers.Tests/GlobalUsings.cs
+++ tests/EffectiveCSharp.Analyzers.Tests/GlobalUsings.cs
@@ -2,3 +2,4 @@ global using EffectiveCSharp.Analyzers.Tests.Helpers;
 global using Microsoft.CodeAnalysis.CodeFixes;
 global using Microsoft.CodeAnalysis.Diagnostics;
 global using Microsoft.CodeAnalysis.Testing;
+global using Xunit.Abstractions;
